27f34a01cd7472211ca35ee9b1a3b7f903c1b0b1
Sambo Chea <sombochea100@gmail.com>
2018-04-04 20:16:49 +0700

    Changed some!

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,18 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js(['resources/assets/js/app.js',
+mix.js('node_modules/jquery/dist/jquery.js','public/js/')
+   .js('resources/assets/js/app.js',
+        'public/js/app.js')
+   .js(['node_modules/chartjs/chart.js',
+        'resources/assets/js/jquery.nicescroll.js',
+        'resources/assets/js/jquery.blockUI.js',
+        'resources/assets/js/detect.js',
+        'resources/assets/js/fastclick.js',
+        'resources/assets/js/pikeadmin.js',
         'node_modules/fullcalendar/dist/fullcalendar.min.js'],
-        'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
+        'public/js/admin.js')
+   .sass('resources/assets/sass/app.scss', 'public/css/app.css')
    .styles([
             'resources/assets/css/fullcalendar_ext.css',
-            'resources/assets/css/style.css'], 'public/css/custom.css');
+            'resources/assets/css/style.css'], 'public/css/admin.css');
