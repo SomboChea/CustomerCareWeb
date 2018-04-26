584bd659d10ce31f03c1b6b06cd5d158c6562814
Sambo Chea <sombochea100@gmail.com>
2018-04-26 08:54:46 +0700

    Ane...

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,15 @@
     </div>
 
     
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+    <script src="{{ mix('js/admin.js') }}" type="text/javascript"></script>
+    
+    <!-- BEGIN Java Script for this page -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+
     <script>
         $(document).ready(function() {
             console.log("jQuery is Activated!");
