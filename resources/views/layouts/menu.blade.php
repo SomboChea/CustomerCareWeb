18be7b00b6b7d8a76f323bad7085c24f9ac9beae
Sambo Chea <sombochea100@gmail.com>
2018-04-26 09:12:47 +0700

    Ane...

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,144 +1,159 @@
 <!-- Left Sidebar -->
 <div class="left main-sidebar">
 
-    <div class="sidebar-inner leftscroll">
+  <div class="sidebar-inner leftscroll">
 
-      <!-- Side bar [Menu] -->
-      <div id="sidebar-menu">
-        <ul>
-          <li class="submenu">
-            <a class="active" href="index.html">
-              <i class="fa fa-fw fa-bars"></i>
-              <span> Calling Lists </span>
-            </a>
-          </li>
-          <li class="submenu">
+    <!-- Side bar [Menu] -->
+    <div id="sidebar-menu">
+      <ul>
+        <li class="submenu">
+          <a class="active" href="index.html">
+            <i class="fa fa-fw fa-bars"></i>
+            <span> Dashboard </span>
+          </a>
+        </li>
+        <li class="submenu">
+          <a href="#">
+            <i class="fa fa-fw fa-area-chart"></i>
+            <span> Calling Lists </span>
+            <span class="menu-arrow"></span>
+          </a>
+          <ul class="list-unstyled">
+              <li>
+                  <a href="#">Pregnent
+                    <span class="label radius-circle bg-primary float-right">10</span>
+                  </a>
+              </li>
+              <li>
+                  <a href="#">Step
+                      <span class="label radius-circle bg-warning float-right">15</span>
+                  </a>
+              </li>
+          </ul>
+        </li>
+        <li class="submenu">
+          <a href="#">
+            <i class="fa fa-fw fa-area-chart"></i>
+            <span> Customers </span>
+            <span class="menu-arrow"></span>
+          </a>
+          <ul class="list-unstyled">
+              <li>
+                  <a href="#">Mom</a>
+              </li>
+              <li>
+                  <a href="#">Kid</a>
+              </li>
+          </ul>
+        </li>
+        <li class="submenu">
+          <a href="#">
+            <i class="fa fa-fw fa-table"></i>
+            <span> Sources </span>
+            <span class="menu-arrow"></span>
+          </a>
+          <ul class="list-unstyled">
+              <li>
+                  <a href="#">Hospital / Clinic</a>
+              </li>
+              <li>
+                  <a href="#">Retail</a>
+              </li>
+              <li>
+                  <a href="#">PC</a>
+              </li>
+              <li>
+                  <a href="#">Other</a>
+              </li>
+          </ul>
+        </li>
+        <li class="submenu">
+          <a href="#">
+            <i class="fa fa-fw fa-th"></i>
+            <span> Products </span>
+            <span class="menu-arrow"></span>
+          </a>
+          <ul class="list-unstyled">
+            <li>
+              <a href="#">Add New</a>
+            </li>
+            <li>
+              <a href="#">View All</a>
+            </li>
+          </ul>
+        </li>
+        <li class="submenu">
+          <a href="#">
+            <span class="label radius-circle bg-danger float-right">20</span>
+            <i class="fa fa-fw fa-copy"></i>
+            <span> Reports </span>
+          </a>
+          <ul class="list-unstyled">
+            <li>
+              <a href="#">Daily</a>
+            </li>
+            <li>
+              <a href="#">Weekly</a>
+            </li>
+          </ul>
+        </li>
+        <li class="submenu">
             <a href="#">
               <i class="fa fa-fw fa-area-chart"></i>
-              <span> Customers </span>
-            </a>
-            <ul class="list-unstyled">
-                <li>
-                    <a href="ui-alerts.html">Mom</a>
-                </li>
-                <li>
-                    <a href="ui-alerts.html">Kid</a>
-                </li>
-            </ul>
-          </li>
-          <li class="submenu">
-            <a href="#">
-              <i class="fa fa-fw fa-table"></i>
-              <span> Sources </span>
+              <span> Users </span>
               <span class="menu-arrow"></span>
             </a>
             <ul class="list-unstyled">
                 <li>
-                    <a href="#">Hospital / Clinic</a>
+                    <a href="#">New user</a>
                 </li>
                 <li>
-                    <a href="#">Retail</a>
+                    <a href="#">View all</a>
                 </li>
                 <li>
-                    <a href="#">PC</a>
+                    <a href="#">Roles</a>
+                </li>
+            </ul>
+          </li>
+        <li class="submenu">
+          <a href="#">
+            <span class="label radius-circle bg-primary float-right">9</span>
+            <i class="fa fa-fw fa-indent"></i>
+            <span> Preferences </span>
+          </a>
+          <ul>
+            <li class="submenu">
+              <a href="#">
+                <span>Alerts</span>
+                <span class="menu-arrow"></span>
+              </a>
+              <ul style="">
+                <li>
+                  <a href="#">
+                    <span>Pregnent</span>
+                  </a>
                 </li>
                 <li>
-                    <a href="#">Other</a>
-                </li>
-            </ul>
-          </li>
-          <li class="submenu">
-            <a href="#">
-              <i class="fa fa-fw fa-tv"></i>
-              <span> User Interface </span>
-              <span class="menu-arrow"></span>
-            </a>
-            <ul class="list-unstyled">  
-              <li>
-                <a href="ui-alerts.html">Alerts</a>
-              </li>
-            </ul>
-          </li>
-          <li class="submenu">
-            <a href="#">
-              <i class="fa fa-fw fa-file-text-o"></i>
-              <span> Forms </span>
-              <span class="menu-arrow"></span>
-            </a>
-            <ul class="list-unstyled">
-              <li>
-                <a href="forms-general.html">General Elements</a>
-              </li>
-            </ul>
-          </li>
-          <li class="submenu">
-            <a href="#">
-              <i class="fa fa-fw fa-th"></i>
-              <span> Plugins </span>
-              <span class="menu-arrow"></span>
-            </a>
-            <ul class="list-unstyled">
-              <li>
-                <a href="star-rating.html">Star Rating</a>
-              </li>
-            </ul>
-          </li>
-          <li class="submenu">
-            <a href="#">
-              <i class="fa fa-fw fa-image"></i>
-              <span> Images and Galleries </span>
-              <span class="menu-arrow"></span>
-            </a>
-            <ul class="list-unstyled">
-              <li>
-                <a href="media-fancybox.html">
-                  <span class="label radius-circle bg-danger float-right">cool</span> Fancybox </a>
-              </li>
-            </ul>
-          </li>
-          <li class="submenu">
-            <a href="#">
-              <span class="label radius-circle bg-danger float-right">20</span>
-              <i class="fa fa-fw fa-copy"></i>
-              <span> Example Pages </span>
-            </a>
-            <ul class="list-unstyled">
-              <li>
-                <a href="page-pricing-tables.html">Pricing Tables</a>
-              </li>
-            </ul>
-          </li>
-          <li class="submenu">
-            <a href="#">
-              <span class="label radius-circle bg-primary float-right">9</span>
-              <i class="fa fa-fw fa-indent"></i>
-              <span> Menu Levels </span>
-            </a>
-            <ul>
-              <li class="submenu">
-                <a href="#">
-                  <span>Third Level</span>
-                  <span class="menu-arrow"></span>
-                </a>
-                <ul style="">
-                  <li>
                     <a href="#">
-                      <span>Third Level Item</span>
+                      <span>Step</span>
                     </a>
                   </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          
-        </ul>
-        <div class="clearfix"></div>
-      </div>
-      <!-- End Side bar [Menu] -->
-
+              </ul>
+            </li>
+            <li>
+                <a href="#">Web Setting</a>
+            </li>
+          </ul>
+        </li>
+        
+      </ul>
       <div class="clearfix"></div>
-
     </div>
+    <!-- End Side bar [Menu] -->
+
+    <div class="clearfix"></div>
 
   </div>
-  <!-- End Left Sidebar -->
+
+</div>
+<!-- End Left Sidebar -->
